7cd85b34e4ab4bf82df0dba2f06fce6391e55662
sac1010 <sachingirish101@gmail.com>
2023-09-14 16:40:53 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import axios from "axios";
 
-// Define debounce function outside the component
 function debounce(func, delay = 1000) {
   let timeoutId;
 
